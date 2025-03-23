1d131b4465580db522a337f2345636d290886a65
joeychen0516 <chen610409@gmail.com>
2025-03-23 20:01:35 +0800

    update config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: import.meta.env.PROD === 'production' ? '/ReactFinal/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/ReactFinal/' : '/',
   plugins: [react()],
 })
